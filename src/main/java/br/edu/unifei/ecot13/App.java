33c11b08010d2d4f4838a78b27aaeea770dd7eb0
Fabio Amorelli <amorelli.ff@gmail.com>
2023-12-14 08:51:41 -0300

    add final presentation adjustments

--- src/main/java/br/edu/unifei/ecot13/App.java
+++ src/main/java/br/edu/unifei/ecot13/App.java
@@ -2,14 +2,17 @@ package br.edu.unifei.ecot13;
 
 import java.sql.Date;
 import java.util.Arrays;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import br.edu.unifei.ecot13.model.Academia;
+import br.edu.unifei.ecot13.model.Arbitro;
 import br.edu.unifei.ecot13.model.Atleta;
 import br.edu.unifei.ecot13.model.Bulking;
+import br.edu.unifei.ecot13.model.Campeonato;
 import br.edu.unifei.ecot13.model.Carboidrato;
 import br.edu.unifei.ecot13.model.CategoriaEnum;
 import br.edu.unifei.ecot13.model.DietaBulking;
@@ -19,65 +22,64 @@ import br.edu.unifei.ecot13.model.Proteina;
 
 public class App {
     public static void main(String[] args) {
-        // EntityManagerFactory entityManagerFactory =
-        // Persistence.createEntityManagerFactory("fisiculturismoPU");
-        // EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("fisiculturismoPU");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        // entityManager.getTransaction().begin();
+        entityManager.getTransaction().begin();
 
-        // // Setup atleta
-        // Atleta atleta = new Atleta(new Bulking());
-        // atleta.setNome("Fulano");
-        // atleta.setCpf("100-200-300-40");
-        // atleta.setNascimento(new Date(850315440000l));
-        // atleta.setSexo('M');
-        // atleta.setPeso(90);
-        // atleta.setAltura(180);
-        // atleta.setCategoria(CategoriaEnum.MENS_CLASSIC);
+        // Setup atleta
+        Atleta atleta = new Atleta(new Bulking());
+        atleta.setNome("Fulano");
+        atleta.setCpf("100.200.300-40");
+        atleta.setNascimento(new Date(850315440000l));
+        atleta.setSexo('M');
+        atleta.setPeso(90);
+        atleta.setAltura(180);
+        atleta.setCategoria(CategoriaEnum.MENS_CLASSIC);
 
-        // Academia academia = new Academia();
-        // academia.setNome("CT do Fulano");
-        // academia.setCnpj("10.200.300/0001-4");
+        Academia academia = new Academia();
+        academia.setNome("CT do Fulano");
+        academia.setCnpj("10.200.300/0001-4");
 
-        // // Setup alimentos
-        // Carboidrato arroz = new Carboidrato();
-        // arroz.setAcucar(false);
-        // arroz.setQuantidade(8 * atleta.getPeso());
+        // Setup alimentos
+        Carboidrato arroz = new Carboidrato();
+        arroz.setAcucar(false);
+        arroz.setQuantidade(8 * atleta.getPeso());
 
-        // Carboidrato mel = new Carboidrato();
-        // mel.setAcucar(true);
-        // mel.setQuantidade(30);
+        Carboidrato mel = new Carboidrato();
+        mel.setAcucar(true);
+        mel.setQuantidade(30);
 
-        // Proteina frango = new Proteina();
-        // frango.setOrigem("animal");
-        // frango.setQuantidade(2 * atleta.getPeso());
+        Proteina frango = new Proteina();
+        frango.setOrigem("animal");
+        frango.setQuantidade(2 * atleta.getPeso());
 
-        // Gordura azeite = new Gordura();
-        // azeite.setQuantidade(0.4f * atleta.getPeso());
+        Gordura azeite = new Gordura();
+        azeite.setQuantidade(0.4f * atleta.getPeso());
 
-        // // Dietas
-        // DietaBulking dietaBulking = new DietaBulking();
-        // dietaBulking.setAlimentos(Arrays.asList(arroz, mel, frango, azeite));
+        // Dietas
+        DietaBulking dietaBulking = new DietaBulking();
+        dietaBulking.setAlimentos(Arrays.asList(arroz, mel, frango, azeite));
 
-        // DietaCutting dietaCutting = new DietaCutting();
-        // dietaCutting.setAlimentos(Arrays.asList(arroz, mel, frango, azeite));
+        DietaCutting dietaCutting = new DietaCutting();
+        dietaCutting.setAlimentos(Arrays.asList(arroz, mel, frango, azeite));
 
-        // atleta.trocarDeFase();
+        atleta.trocarDeFase();
 
-        // entityManager.persist(arroz);
-        // entityManager.persist(mel);
-        // entityManager.persist(frango);
-        // entityManager.persist(azeite);
-        // entityManager.persist(dietaBulking);
-        // // entityManager.persist(dietaCutting);
+        entityManager.persist(arroz);
+        entityManager.persist(mel);
+        entityManager.persist(frango);
+        entityManager.persist(azeite);
+        entityManager.persist(dietaBulking);
+        // entityManager.persist(dietaCutting);
 
-        // entityManager.persist(academia);
-        // entityManager.persist(atleta.getFase());
-        // entityManager.persist(atleta);
+        entityManager.persist(academia);
+        entityManager.persist(atleta.getFase());
+        entityManager.persist(atleta);
 
-        // entityManager.getTransaction().commit();
-        // entityManager.close();
-        // entityManagerFactory.close();
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        entityManagerFactory.close();
 
         GUIStarter.main(args);
     }
